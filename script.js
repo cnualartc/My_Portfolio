38ee42028f5183062fd3b8511190547063d952b6
Christian Nualart <nualart@Christians-Mac-mini.local>
2023-01-30 15:23:38 -0600

    Correct linter error

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-let menu = document.getElementById("hamburger");
-let wrapper = document.querySelector(".top");
-let nav = document.querySelector(".menu");
-let listWrapper = document.querySelector(".desktop-menu-list-container");
-let list = document.querySelector(".desktop-menu-list");
+const menu = document.getElementById('hamburger');
+const wrapper = document.querySelector('.top');
+const nav = document.querySelector('.menu');
+const listWrapper = document.querySelector('.desktop-menu-list-container');
+const list = document.querySelector('.desktop-menu-list');
+
+function showMobileMenu() {
+  wrapper.classList.add('mobile-button-clicked');
+  nav.classList.add('mobile-navbar');
+  listWrapper.classList.add('visible');
+  list.classList.add('mobile-menus');
+  menu.classList.add('cancel-button');
+  menu.src = '/images/header/icon-cancel.svg';
+}
+
+function hideMobileMenu() {
+  wrapper.classList.remove('mobile-button-clicked');
+  nav.classList.remove('mobile-navbar');
+  listWrapper.classList.remove('visible');
+  list.classList.remove('mobile-menus');
+  menu.classList.remove('cancel-button');
+  menu.src = './images/header/burger_menu.png';
+}
 
 menu.onclick = () => {
-  if (menu.classList.contains("cancel-button")) {
+  if (menu.classList.contains('cancel-button')) {
     hideMobileMenu();
   } else {
     showMobileMenu();
   }
 };
 
-function showMobileMenu() {
-  wrapper.classList.add("mobile-button-clicked");
-  nav.classList.add("mobile-navbar");
-  listWrapper.classList.add("visible");
-  list.classList.add("mobile-menus");
-  menu.classList.add("cancel-button");
-  menu.src = "/images/header/icon-cancel.svg";
-}
-
-function hideMobileMenu() {
-  wrapper.classList.remove("mobile-button-clicked");
-  nav.classList.remove("mobile-navbar");
-  listWrapper.classList.remove("visible");
-  list.classList.remove("mobile-menus");
-  menu.classList.remove("cancel-button");
-  menu.src = "./images/header/burger_menu.png";
-}
